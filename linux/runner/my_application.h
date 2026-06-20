a35f56de432041fffb53943e3855aace7fd4fc5f
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2026-06-20 18:27:50 -0500

    Create my_application.h

---
+++ linux/runner/my_application.h
@@ -0,0 +1,10 @@
+#ifndef MY_APPLICATION_H_
+#define MY_APPLICATION_H_
+
+#include <gtk/gtk.h>
+
+G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION, GtkApplication)
+
+MyApplication* my_application_new();
+
+#endif
