7748435f3f943ef439a65d0f05be079f71f9df8a
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2026-06-20 18:57:47 -0500

    Create generated_plugin_registrant.h

---
+++ windows/flutter/generated_plugin_registrant.h
@@ -0,0 +1,8 @@
+#ifndef GENERATED_PLUGIN_REGISTRANT_H_
+#define GENERATED_PLUGIN_REGISTRANT_H_
+
+#include <flutter/plugin_registry.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry);
+
+#endif
