3cf8d48d3d4112e08a515c2eb565bea41d42816c
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2026-06-20 18:21:29 -0500

    Create db_service.dart

---
+++ lib/services/db_service.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import '../models/book.dart';
+
+class DbService {
+  static Database? _db;
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDb();
+    return _db!;
+  }
+
+  Future<Database> _initDb() async {
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+    }
+
+    String path = join(await getDatabasesPath(), 'tech_inventory.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE equipos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        codigo TEXT, 
+        nombre TEXT, 
+        descripcion TEXT,
+        fecha TEXT,
+        foto TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertEquipo(Equipo equipo) async {
+    final db = await database;
+    return await db.insert(
+      'equipos', 
+      equipo.toMap(), 
+      conflictAlgorithm: ConflictAlgorithm.replace
+    );
+  }
+
+  Future<List<Equipo>> getEquipos() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('equipos', orderBy: 'id DESC');
+    return List.generate(maps.length, (i) {
+      return Equipo.fromMap(maps[i]);
+    });
+  }
+
+  Future<int> deleteEquipo(int id) async {
+    final db = await database;
+    return await db.delete(
+      'equipos',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
