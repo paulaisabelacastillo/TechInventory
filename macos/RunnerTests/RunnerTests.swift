6548fa9d3ea3e0a2fc748a131937d992375b0cb1
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2026-06-20 18:47:41 -0500

    Create RunnerTests.swift

---
+++ macos/RunnerTests/RunnerTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+class RunnerTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
