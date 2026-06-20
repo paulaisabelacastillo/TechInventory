7ad76757ee34e9a5a110dd49a0c7aebd63a0b775
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2026-06-20 18:26:03 -0500

    Add generated_plugin_registrant.h for plugin registration

---
+++ linux/flutter/generated_plugin_registrant.h
@@ -0,0 +1,8 @@
+#ifndef GENERATED_PLUGIN_REGISTRANT_H_
+#define GENERATED_PLUGIN_REGISTRANT_H_
+
+#include <flutter_linux/flutter_linux.h>
+
+void fl_register_plugins(FlPluginRegistry* registry);
+
+#endif
