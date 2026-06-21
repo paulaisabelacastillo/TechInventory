d3732eaff85b2eff7feb934683d3fff29fbf5572
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2026-06-20 19:00:03 -0500

    Create flutter_window.cpp

---
+++ windows/runner/flutter_window.cpp
@@ -0,0 +1,33 @@
+#include "flutter_window.h"
+#include <optional>
+#include "flutter/generated_plugin_registrant.h"
+
+FlutterWindow::FlutterWindow(const flutter::DartProject& project)
+    : project_(project) {}
+
+FlutterWindow::~FlutterWindow() {}
+
+bool FlutterWindow::OnCreate() {
+  if (!Win32Window::OnCreate()) {
+    return false;
+  }
+
+  RECT frame = GetClientArea();
+  int width = frame.right - frame.left;
+  int height = frame.bottom - frame.top;
+
+  flutter_controller_ = std::make_unique<flutter::FlutterViewController>(width, height, project_);
+  if (!flutter_controller_->engine() || !flutter_controller_->view()) {
+    return false;
+  }
+  RegisterPlugins(flutter_controller_->engine());
+  SetChildContent(flutter_controller_->view()->GetNativeWindow());
+  return true;
+}
+
+void FlutterWindow::OnDestroy() {
+  if (flutter_controller_) {
+    flutter_controller_ = nullptr;
+  }
+  Win32Window::OnDestroy();
+}
